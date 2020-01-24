c8469df4da3dd2793dcac0ea3c5312d3fff17bc7
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2020-01-24 10:56:22 +0100

    DemO

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//5 ITN nr 3 Enrick De Munter
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            Random randomGen = new Random();
+            byte[] lottoCijfers = new byte[6];
+            for (int i = 0; i < 6; i++)
+            {
+                lottoCijfers[1] = (byte)randomGen.Next(1, 43);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(lottoCijfers[1]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("gesorteerde getallen");
+            foreach (var item in lottoCijfers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("aflopend gesorteerde getallen");
+            foreach (var item in lottoCijfers)
+            {
+                Console.WriteLine(item);
+            }
+
+            //wachten
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten");
+            Console.ReadKey();
+
         }
     }
 }
